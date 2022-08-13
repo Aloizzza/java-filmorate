50f9ee7e473dff85a4a22827a095b0dab77c41e5
Aloizzza <aloizzza@yandex.ru>
2022-08-13 15:07:39 +0300

    refactor: финальный проект 9 спринта (исправлено 2)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -6,8 +6,9 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,29 +18,29 @@ public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
     private int id = 0;
 
-    @GetMapping //получение всех фильмов
-    public Collection<Film> findAll() {
-        return films.values();
+    @GetMapping
+    public List<Film> findAll() {
+        return new ArrayList<>(films.values());
     }
 
-    @PostMapping //добавление фильма
+    @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         film.setId(++id);
         films.put(film.getId(), film);
         log.debug("Добавлен новый фильм: {}", film);
+
         return film;
     }
 
-    @PutMapping //обновление фильма
+    @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        if (films.containsKey(film.getId())) {
-            films.put(film.getId(), film);
-            log.debug("Обновлена карточка фильма: {}", film);
-            return film;
-        } else {
-            log.warn("Фильм с указанным id не существует: {}", film.getId());
-            throw new NotFoundException("Фильм с указанным id не существует.");
+        if (!films.containsKey(film.getId())) {
+            throw new NotFoundException("Фильм с указанным id не существует: " + film.getId());
         }
+        films.put(film.getId(), film);
+        log.debug("Обновлена карточка фильма: {}", film);
+
+        return film;
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,13 +2,14 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.*;
+import ru.yandex.practicum.filmorate.exception.ConflictException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,19 +19,13 @@ public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
     private int id = 0;
 
-    @GetMapping //получение списка всех пользователей
-    public Collection<User> findAll() {
-        return users.values();
+    @GetMapping
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
     }
 
-    @PostMapping //создание пользователя
+    @PostMapping
     public User create(@Valid @RequestBody User user) {
-        for(User existingUser : users.values()) {
-            if (existingUser.getEmail().equals(user.getEmail())) {
-                log.warn("Указанный адрес электронной почты уже используется.");
-                throw new ConflictException("Указанный адрес электронной почты уже используется.");
-            }
-        }
         if (users.containsKey(user.getId())) {
             throw new ConflictException("Указанный id уже используется");
         }
@@ -40,21 +35,27 @@ public class UserController {
         user.setId(++id);
         users.put(user.getId(), user);
         log.debug("Добавлен новый пользователь: {}", user);
+
         return user;
     }
 
-    @PutMapping //обновление пользователя
+    @PutMapping
     public User update(@Valid @RequestBody User user) {
-        if (users.containsKey(user.getId())) {
-            if (user.getName().isBlank()) {
-                user.setName(user.getLogin());
-            }
-            users.put(user.getId(), user);
-            log.debug("Обновлена карточка пользователя: {}", user);
-            return user;
-        } else {
-            log.warn("Пользователя с указанным id не существует: {}", user.getId());
-            throw new NotFoundException("Пользователя с указанным id не существует.");
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("Не существует пользователя с указанным id: " + user.getId());
+        }
+
+        defineUserName(user);
+
+        users.put(user.getId(), user);
+        log.debug("Обновлена карточка пользователя: {}", user);
+
+        return user;
+    }
+
+    private void defineUserName(User user) {
+        if (user.getName().isBlank()) {
+            user.setName(user.getLogin());
         }
     }
 
